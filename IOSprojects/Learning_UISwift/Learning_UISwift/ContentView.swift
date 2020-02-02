50bf3a5a4c16e8fe1c806ea3288147b1e57d79b8
Chinwa Williams <chinwawilliams@gmail.com>
2020-02-02 02:23:46 +0100

    Added Layout components

--- IOSprojects/Learning_UISwift/Learning_UISwift/ContentView.swift
+++ IOSprojects/Learning_UISwift/Learning_UISwift/ContentView.swift
@@ -1,39 +1,71 @@
-//
-//  ContentView.swift
-//  Learning_UISwift
-//
-//  Created by Chinwa Williams on 26/01/2020.
-//  Copyright © 2020 Chinwa Williams. All rights reserved.
-//
+            //
+            //  ContentView.swift
+            //  Learning_UISwift
+            //
+            //  Created by Chinwa Williams on 26/01/2020.
+            //  Copyright © 2020 Chinwa Williams. All rights reserved.
+            //
 
-import SwiftUI
+            import SwiftUI
 
-struct ContentView: View {
-    @State var showAlert: Bool = false;
-    
-    var body: some View {
-       
-            
-            //Header row
-         VStack {
-                   HStack {
-                       Text("Put the bullseye as close as you can to:")
-                   }
-            
-            //Slider row
-            
-            //button row
-            
-            //score row
+            struct ContentView: View {
+                @State var sliderValue = 50.0;
                 
-            
-        }
-    }
-}
+                
+                var body: some View {
+                    VStack {
+                        
+                        HStack {
+                            
+                           Text("Put the bullseye as close as you can to:")
+                          Text("100")
+                            }
+                           .padding(.init(top: 50, leading: 0, bottom: 0, trailing: 0))
+                            Spacer()
+                                   
+                        VStack{
+                            HStack {
+                                 Text("0")
+                                     Slider(value: $sliderValue, in: 1...100)
+                                      Text("100")
+                                   }
+                                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                                     Text("Hit me")
+                                   }
+                                                   
+                        }
+                            
+                           Spacer()
+                        
+                        HStack{
+                            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                                Text("Start Over")
+                            }
+                            
+                            Spacer()
+                             Text("Score:")
+                             Text("999999")
+                               Spacer()
+                            Text("Round:")
+                            Text("999")
+                            
+                               Spacer()
+                            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                                Text("Info")
+                            }
+                            
+                        }
+                        
+                        .padding(.init(top: 0, leading: 0, bottom: 20, trailing: 0))
+                        
+                   
+                        
+                    }
+                }
+            }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .previewLayout(.fixed(width: 896, height: 414))
-    }
-}
+            struct ContentView_Previews: PreviewProvider {
+                static var previews: some View {
+                    ContentView().previewLayout(.fixed(width: 896, height: 414))
+                }
+            }
